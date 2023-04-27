4b0964b1406ba3fbd7c79b301e1e821d63fe2216
DaiYuANg <dai_yuang@icloud.com>
2023-04-27 23:26:49 +0800

    update io walker

--- libs/io/src/main/java/org/daiyuang/libs/io/dir/Walker.java
+++ libs/io/src/main/java/org/daiyuang/libs/io/dir/Walker.java
@@ -1,39 +1,60 @@
 package org.daiyuang.libs.io.dir;
 
+import com.google.common.base.Stopwatch;
 import jdk.jfr.Experimental;
+import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.daiyuang.thready.async.AsyncWork;
+import org.daiyuang.thready.pool.PoolCreator;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Objects;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 @Experimental
 @UtilityClass
 @Slf4j
 public class Walker {
-    private final AsyncWork asyncWork = new AsyncWork();
+    private static final AsyncWork asyncWork = new AsyncWork();
 
     public static void main(String[] args) {
+        var s = Stopwatch.createStarted();
         walk(".", file -> {
             log.info(file.getAbsolutePath());
         });
+        s.stop();
+        log.info("spend time:{}", s.elapsed().getSeconds());
     }
 
+    @SneakyThrows
     public static void walk(File directory, Consumer<File> consumer) {
-        if (!directory.isDirectory()) throw new RuntimeException("path is not a directory");
-        var listFile = directory.listFiles();
-        if (listFile != null && listFile.length == 0) return;
-        if (listFile == null) return;
-        //            if (log.isInfoEnabled()) log.info(r.getAbsolutePath());
-        Arrays.stream(listFile).forEach(r -> {
-            if (r.isDirectory()) asyncWork.run(() -> walk(r, consumer));
-            consumer.accept(r);
+        if (Objects.isNull(directory)) throw new NullPointerException("File is null, check your file object?");
+        if (!directory.isDirectory())
+            throw new RuntimeException("Path is not a directory, if not a directory how can I walk through");
+//       todo: do we need parallel?
+        var fileList = Objects.requireNonNull(directory.listFiles());
+        ThreadPoolExecutor e = new PoolCreator().creator();
+        Arrays.stream(fileList).forEach(r -> e.submit(() -> justWalk(r, consumer)));
+        if (e.awaitTermination(fileList.length, TimeUnit.SECONDS)) e.shutdown();
+    }
+
+    private void justWalk(File path, Consumer<File> consumer) {
+        Arrays.stream(Objects.requireNonNull(path.listFiles())).forEach(r -> {
+            if (r.isDirectory()) justWalk(r, consumer);
+            if (r.canRead()) consumer.accept(r);
         });
     }
 
     public static void walk(String path, Consumer<File> consumer) {
         walk(new File(path), consumer);
+    }
+
+    //    todo make concurrent with thread
+    public static void concurrentWalk(String path, Consumer<File> consumer) {
+//        walk(path);
     }
 }
