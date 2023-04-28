0a0284f9b08790eaf96f2d64ee200f38f1e258cc
DaiYuANg <dai_yuang@icloud.com>
2023-04-28 14:56:32 +0800

    更新walk

--- libs/io/src/main/java/org/daiyuang/libs/io/dir/Walker.java
+++ libs/io/src/main/java/org/daiyuang/libs/io/dir/Walker.java
@@ -4,34 +4,40 @@ import jdk.jfr.Experimental;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
-import org.daiyuang.thready.pool.PoolCreator;
+import org.daiyuang.thready.async.AsyncWork;
 
 import java.io.File;
 import java.util.Arrays;
 import java.util.Objects;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 @Experimental
 @UtilityClass
 @Slf4j
 public class Walker {
+    AsyncWork asyncWork = new AsyncWork();
+
+    public static void main(String[] args) {
+        walk("/Users/daiyuang/Projects/Aquarius", file -> {
+            log.info(file.getAbsolutePath());
+        });
+    }
+
     @SneakyThrows
     public static void walk(File directory, Consumer<File> consumer) {
         if (Objects.isNull(directory)) throw new NullPointerException("File is null, check your file object?");
         if (!directory.isDirectory())
             throw new RuntimeException("Path is not a directory, if not a directory how can I walk through");
-//       todo: do we need parallel?
         var fileList = Objects.requireNonNull(directory.listFiles());
-        ThreadPoolExecutor e = new PoolCreator().creator();
-        Arrays.stream(fileList).forEach(r -> e.submit(() -> justWalk(r, consumer)));
-        if (e.awaitTermination(fileList.length, TimeUnit.SECONDS)) e.shutdown();
+        Arrays.stream(fileList).forEach(r -> {
+            if (Objects.nonNull(r.listFiles()) && Objects.requireNonNull(r.listFiles()).length > 0)
+                asyncWork.run(() -> justWalk(r, consumer));
+        });
     }
 
     private void justWalk(File path, Consumer<File> consumer) {
         Arrays.stream(Objects.requireNonNull(path.listFiles())).forEach(r -> {
-            if (r.isDirectory()) justWalk(r, consumer);
+            if (r.isDirectory()) asyncWork.run(() -> justWalk(r, consumer));
             if (r.canRead()) consumer.accept(r);
         });
     }
