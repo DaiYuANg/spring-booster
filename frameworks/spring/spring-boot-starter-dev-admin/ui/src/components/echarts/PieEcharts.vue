cc563bc27f1418deae352e61c70c9d9f7720a757
DaiYuANg <dai_yuang@icloud.com>
2023-07-09 17:55:47 +0800

    update echarts component

--- frameworks/spring/spring-boot-starter-dev-admin/ui/src/components/echarts/PieEcharts.vue
+++ frameworks/spring/spring-boot-starter-dev-admin/ui/src/components/echarts/PieEcharts.vue
@@ -1,5 +1,5 @@
 <template>
-<div :id="props.idName"></div>
+  <div :id="props.idName"></div>
 </template>
 
 <script setup lang="ts">
@@ -7,46 +7,37 @@
  * @idName  Html id name
  * @title Title property
  */
-import echarts from 'boot/echarts';
-import { EChartsOption } from 'echarts';
-import {onMounted} from "vue";
+import echarts, { ECOption } from 'boot/echarts';
+import { onMounted } from 'vue';
 
 const props = defineProps({
   idName: {
     type: String,
     required: true,
   },
-  title: {
-    type: Object,
-  },
-  tooltip: {
+  option: {
     type: Object,
   },
 });
 
-
-const option:EChartsOption = {};
-
-//Option Object assignment
-const setOption = () =>{
-  if(props.title !== undefined){
-      option.title = props.title;
-  }
-
-}
-
 //The Echarts property is assigned a value
-const initEcharts = ()=>{
+const initEcharts = () => {
   let _charDom = document.getElementById(props.idName) as HTMLElement;
   let _myChar = echarts.init(_charDom);
-  setOption();
-  _myChar.setOption(option)
-}
+  if (props.option === undefined) {
+    return;
+  }
+  _myChar.setOption(props.option as ECOption);
+  window.onresize = function () {
+    //自适应大小
+    _myChar.resize();
+  };
+};
 
-onMounted(()=>{
+onMounted(() => {
   //initEcharts call
   initEcharts();
-})
+});
 </script>
 
 <style scoped></style>
