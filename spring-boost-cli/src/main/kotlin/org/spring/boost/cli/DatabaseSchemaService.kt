5ec7ce63d74b9ba975ed81720a44dd0cdf3a1b3c
DaiYuANg <dai_yuang@icloud.com>
2024-01-22 11:51:50 +0800

    feat(root): update versions

--- spring-boost-cli/src/main/kotlin/org/spring/boost/cli/DatabaseSchemaService.kt
+++ spring-boost-cli/src/main/kotlin/org/spring/boost/cli/DatabaseSchemaService.kt
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service
 import java.util.concurrent.StructuredTaskScope
 import javax.sql.DataSource
 
-
 @Service
 class DatabaseSchemaService(
     private val dataSource: DataSource,
