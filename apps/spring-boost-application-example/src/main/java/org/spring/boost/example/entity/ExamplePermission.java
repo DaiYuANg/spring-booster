4666b7ef5c1feb6dce565ae90ad992f710b5db0a
DaiYuANg <138393979+DaiYuANg@users.noreply.github.com>
2024-11-20 10:15:25 +0800

    feat(project): upgrade versions

---
+++ apps/spring-boost-application-example/src/main/java/org/spring/boost/example/entity/ExamplePermission.java
@@ -0,0 +1,11 @@
+package org.spring.boost.example.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import org.spring.boost.rbac.entity.SysPermission;
+
+@Entity
+public class ExamplePermission extends SysPermission {
+
+}
